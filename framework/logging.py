c2ad04cdbd70912c78f4f26ed54e698921314045
Cheng Zhang <zhangchengx@gmail.com>
2021-08-30 14:49:38 -0400

    Update logging.py

--- framework/logging.py
+++ framework/logging.py
@@ -3,58 +3,19 @@ import logging.config
 
 LOGGING_CONFIG = {
     'version': 1,
-    'loggers': {
-        '': {  # root logger
-            'level': 'NOTSET',
-            'handlers': ['debug_console_handler', 'info_rotating_file_handler', 'error_file_handler', 'critical_mail_handler'],
-        },
-        'warning': { 
-            'level': 'WARNING',
-            'propagate': False,
-            'handlers': ['info_rotating_file_handler', 'error_file_handler' ],
-        },
-        'error': { 
-            'level': 'ERROR',
-            'propagate': False,
-            'handlers': ['info_rotating_file_handler', 'error_file_handler' ],
-        },
-    },
-    'handlers': {
-        'debug_console_handler': {
-            'level': 'DEBUG',
-            'formatter': 'info',
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://sys.stdout',
-        },
-        'info_rotating_file_handler': {
-            'level': 'INFO',
-            'formatter': 'info',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': 'info.log',
-            'mode': 'a',
-            'maxBytes': 1048576,
-            'backupCount': 10
-        },
-        'error_file_handler': {
-            'level': 'WARNING',
-            'formatter': 'error',
-            'class': 'logging.FileHandler',
-            'filename': 'error.log',
-            'mode': 'a',
-        },
-        'critical_mail_handler': {
-            'level': 'CRITICAL',
-            'formatter': 'error',
-            'class': 'logging.handlers.SMTPHandler',
-            'mailhost' : 'localhost',
-            'fromaddr': 'monitoring@example.com',
-            'toaddrs': ['dev@example.com', 'qa@example.com'],
-            'subject': 'Critical error with application name'
-        }
-    },
+    'disable_existing_loggers': True,
     'formatters': {
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(name)s %(module)s:%(process)d %(thread)d %(message)s'
+        },
+        'development': {
+            'format': '%(levelname)s: %(module)s:%(lineno)s | %(message)s'
+        },
         'info': {
-            'format': '%(levelname)s:   %(message)s'
+            'format': '%(levelname)s: %(module)s:%(lineno)s | %(message)s'
         },
         'warning': {
             'format': '%(levelname)s: %(asctime)s | %(name)s | %(module)s:%(lineno)s | %(message)s'
@@ -63,11 +24,59 @@ LOGGING_CONFIG = {
             'format': '%(levelname)s: %(asctime)s | %(name)s | %(module)s:%(lineno)s | %(message)s'
         },
     },
-
+    'handlers': {
+        'simple': {
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'development',
+            'stream': 'ext://sys.stdout',
+        },
+        'file': {
+            'level': 'WARNING',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'error',
+            'filename': 'error.log',
+            'mode': 'a',
+            'maxBytes': 1048576,
+            'backupCount': 10,
+        },
+        'mail': {
+            'level': 'CRITICAL',
+            'class': 'logging.handlers.SMTPHandler',
+            'mailhost' : 'localhost',
+            'fromaddr': 'monitoring@example.com',
+            'toaddrs': ['dev@example.com', 'support@example.com'],
+            'subject': 'Critical error with application name',
+        }
+    },
+    'loggers': {
+        '': { # root logger
+            'handlers':['console'],
+            'level':'NOTSET',
+            'propagate': True,
+        },
+        'warning': { 
+            'handlers': ['file', 'console' ],
+            'level': 'WARNING',
+            'propagate': False,
+        },
+        'error': { 
+            'handlers': ['file', 'console' ],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
 }
 
 logging.config.dictConfig(LOGGING_CONFIG)
 
 logger = logging.getLogger('')
+
+warning_logger = logging.getLogger('warning')
 
 error_logger = logging.getLogger('error')
