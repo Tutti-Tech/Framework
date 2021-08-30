b0f3b06a5c53db31d310f364c1ab528c277c295f
Cheng Zhang <zhangchengx@gmail.com>
2021-08-30 15:20:13 -0400

    Create request.py

---
+++ framework/request.py
@@ -0,0 +1,4 @@
+from starlette.requests import Request
+
+class Request(Request):
+    pass
--- tests/test_application.py
+++ tests/test_application.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append("..")
 
 from framework.application import Application
+from framework.request import Request
 from framework.response import text, html
 
 app = Application()
@@ -11,5 +12,5 @@ def home(request):
     return text('Home Page')
 
 @app.route('/hello/{username}')
-def hello(request):
-    return html('Hello, <strong>%s</strong>' % request.path_params['username'])
+def hello(request: Request):
+    return html('Hello, <strong>%s</strong> <br> Your IP: %s ' % (request.path_params['username'], request.client.host))
