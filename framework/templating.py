e3560b40e4a767f327c38a02508b9d12b25dd1aa
Cheng Zhang <zhangchengx@gmail.com>
2021-09-01 12:55:19 -0400

    Pass request to templating environment

--- framework/templating.py
+++ framework/templating.py
@@ -1,14 +1,26 @@
+import typing
 from jinja2 import Environment, FileSystemLoader
+
+from framework.request import Request
 from framework.response import html
 
 
 class Templating():
     
-    def __init__(self, directory: str = 'templates') -> None:
+    def __init__(self, directory: str = 'templates', request: Request = None) -> None:
         self.env = self._create_env(directory)
+        if request:
+            self.env = self._create_request_env(request)
 
     def _create_env(self, directory: str):
         return Environment(loader=FileSystemLoader(directory), autoescape=True)
+    
+    def _create_request_env(self, request: Request):
+        self.env.globals["base_url"] = request.base_url
+        self.env.globals["app_url"] = request.app_url
+        self.env.globals["static_url"] = request.url_for('static', path='/')
+        self.env.globals["url_for"] = request.url_for
+        return self.env
     
     def __call__(self, template_name: str, context: dict) -> None:
         template = self.env.get_template(template_name)
